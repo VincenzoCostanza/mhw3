3fd039254f147f32f4f2d7ad909122102533ba01
VincenzoCostanza <81384570+VincenzoCostanza@users.noreply.github.com>
2021-04-26 16:26:33 +0200

    Add files via upload

---
+++ contents.js
@@ -0,0 +1,38 @@
+const contenuti = [
+    {
+        titolo: 'Serie A',
+        immagine: 'seriea.png',
+        descrizione: 'Campionato italiano'
+    },
+
+    {
+        titolo: 'Premier League',
+        immagine: 'premierleague.png',
+        descrizione: 'Campionato con il fiato sospeso'   
+    },
+
+    {
+        titolo: 'Champions League',
+        immagine: 'champions.jpg',
+        descrizione: 'La coppa dalle grandi orecchie'
+    },
+
+    {
+        titolo: 'Europa League',
+        immagine: 'europa.jpg',
+        descrizione: 'La coppa uefa per gli intenditori'   
+    },
+
+    {
+        titolo: 'Liga',
+        immagine: 'liga.jpg',
+        descrizione: 'Campionato spagnolo'   
+    },
+
+    {
+        titolo: 'Bundesliga',
+        immagine: 'bunde.png',
+        descrizione: 'Campionato tedesco'   
+    }
+
+]
